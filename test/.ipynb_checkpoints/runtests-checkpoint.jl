3505595f4a1baedcc8a8bf1781c9c46cb0f9527d
kanamaru-masanori <kanamaru-masanori@hotmail.co.jp>
2021-04-06 17:33:30 +0900

    Update test
    Update test

--- test/.ipynb_checkpoints/runtests-checkpoint.jl
+++ test/.ipynb_checkpoints/runtests-checkpoint.jl
@@ -3,7 +3,7 @@ using Test
 
 @testset "Astroshaper.jl" begin
     shapepath = "Julia_Vernazza_2018.obj"
-    shape = setShapeModel(shapepath; scale=1000)
-    findVisibleFaces!(shape)
+    @test shape = setShapeModel(shapepath; scale=1000)
+    @test findVisibleFaces!(shape)
 end
 
