1cf7552b2f81f066284f6ed32b5a5e0050e6422e
kanamaru-masanori <kanamaru-masanori@hotmail.co.jp>
2021-04-06 17:38:37 +0900

    Update test

--- test/.ipynb_checkpoints/runtests-checkpoint.jl
+++ test/.ipynb_checkpoints/runtests-checkpoint.jl
@@ -2,7 +2,7 @@ using Astroshaper
 using Test
 
 @testset "Astroshaper.jl" begin
-    shapepath = "itokawa_v2718_f5430.obj"
+    shapepath = "./itokawa_v2718_f5430.obj"
     @test shape = setShapeModel(shapepath; scale=1000)
     @test findVisibleFaces!(shape)
 end
