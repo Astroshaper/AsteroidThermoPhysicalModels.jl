67c08794ee02cfe6e6910f537c9eb5f50175e13a
M. Kanamaru <kanamaru-masanori@hotmail.co.jp>
2021-04-28 16:07:23 +0900

    Update test

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,7 +4,7 @@ using Test
 @testset "Astroshaper.jl" begin
     shapepath = "./ryugu_test.obj"
     shape = setShapeModel(shapepath; scale=1000)
-    @time sumTorqueOverSurface(shape, 1200, [1,0,0.])
+    @time Astroshaper.sumTorqueOverSurface(shape, 1200, [1,0,0.])
     @test true
 end
 
