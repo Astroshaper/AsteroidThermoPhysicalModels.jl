51d73bda89869703b969d7b09b9e159365d33521
kanamaru-masanori <kanamaru-masanori@hotmail.co.jp>
2021-05-06 16:43:43 +0900

    Self-sahdowing

--- src/.ipynb_checkpoints/smesh-checkpoint.jl
+++ src/.ipynb_checkpoints/smesh-checkpoint.jl
@@ -159,7 +159,7 @@ function raycast(A, B, C, R)
     t = (Q ⋅ E2) / P_dot_E1  # Distance to triangle (if t < 0, the ray hits from the back)
 
     # 0 ≤ u ≤ 1 && 0 ≤ v ≤ 1 && 0 ≤ u + v ≤ 1 && t > 0 && (return true)
-    0 ≤ u + v ≤ 1 && 0 ≤ u ≤ 1 && 0 ≤ v ≤ 1 && t > 0 && (return true)
+    t > 0 && 0 ≤ u ≤ 1 && 0 ≤ v ≤ 1 && 0 ≤ u + v ≤ 1 && (return true)
     return false
 end
 
