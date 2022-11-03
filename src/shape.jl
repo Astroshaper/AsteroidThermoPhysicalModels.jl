4d4e142a666f4e32c51eade42abcd85aae125b1c
kanamaru-masanori <kanamaru-masanori@hotmail.co.jp>
2022-11-04 06:24:11 +0900

    Revert "Add RADIUS_MIN"
    This reverts commit 637ca3b25ffae74d4027fee0750c1d97edba1c72.

--- src/shape.jl
+++ src/shape.jl
@@ -53,7 +53,6 @@ function Base.show(io::IO, shape::ShapeModel)
     println("Volume            : ", shape.VOLUME)
     println("Equivalent radius : ", shape.RADIUS_EQ)
     println("Maximum radius    : ", shape.RADIUS_MAX)
-    println("Minimum radius    : ", shape.RADIUS_MIN)
     println("Center-of-Figure  : ", shape.COF)
     println("Inertia tensor    : ")
     println("    | Ixx Ixy Ixz |   ", shape.MOI[1, :])
@@ -79,7 +78,6 @@ function ShapeModel(shapepath; scale=1, find_visible_facets=false, save_shape=fa
         VOLUME     = getvolume(facets)
         RADIUS_EQ  = equivalent_radius(VOLUME)
         RADIUS_MAX = maximum_radius(nodes)
-        RADIUS_MIN = minimum_radius(nodes)
         COF        = center_of_figure(facets)
         MOI        = moment_of_inertia(facets)
         
@@ -88,7 +86,7 @@ function ShapeModel(shapepath; scale=1, find_visible_facets=false, save_shape=fa
         
         shape = ShapeModel(
             num_node, num_face, nodes, faces, facets,
-            AREA, VOLUME, RADIUS_EQ, RADIUS_MAX, RADIUS_MIN, COF, MOI,
+            AREA, VOLUME, RADIUS_EQ, RADIUS_MAX, COF, MOI,
             force, torque
         )
         save_shape && save(splitext(shapepath)[1] * ".jld2", Dict("shape" => shape))
@@ -107,9 +105,6 @@ equivalent_radius(shape::ShapeModel) = equivalent_radius(shape.VOLUME)
 
 maximum_radius(nodes) = maximum(norm.(nodes))
 maximum_radius(shape::ShapeModel) = maximum_radius(shape.nodes)
-
-minimum_radius(nodes) = minimum(norm.(nodes))
-minimum_radius(shape::ShapeModel) = minimum_radius(shape.nodes)
 
 findVisibleFacets!(shape::ShapeModel) = findVisibleFacets!(shape.facets)
 isIlluminated(obs::Facet, r̂☉, shape::ShapeModel) = isIlluminated(obs, r̂☉, shape.facets)
