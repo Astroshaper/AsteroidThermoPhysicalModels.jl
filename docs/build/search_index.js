b6d00e539ee1d6372acac22c36a38c70214083fa
hyrodium <hyrodium@gmail.com>
2023-01-07 10:03:27 +0900

    remove docs/build

--- docs/build/search_index.js
+++
@@ -1,3 +0,0 @@
-var documenterSearchIndex = {"docs":
-[{"location":"#Astroshaper.jl-1","page":"Astroshaper.jl","title":"Astroshaper.jl","text":"","category":"section"},{"location":"#","page":"Astroshaper.jl","title":"Astroshaper.jl","text":"A package for dynamical simulation of an asteroid.","category":"page"},{"location":"#","page":"Astroshaper.jl","title":"Astroshaper.jl","text":"Point\ndouble","category":"page"},{"location":"#Astroshaper.Point","page":"Astroshaper.jl","title":"Astroshaper.Point","text":"A point type\n\n\n\n\n\n","category":"type"},{"location":"#Astroshaper.double","page":"Astroshaper.jl","title":"Astroshaper.double","text":"double(p)\n\nDouble p.\n\n\n\n\n\n","category":"function"}]
-}
