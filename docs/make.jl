62756bef096d3f391d615248eda6c4419698431c
hyrodium <hyrodium@gmail.com>
2023-01-11 18:58:16 +0900

    document generation with Documenter.jl

--- docs/make.jl
+++ docs/make.jl
@@ -1,4 +1,22 @@
 using Astroshaper
 using Documenter
 
-makedocs(sitename="Astroshaper.jl")
+DocMeta.setdocmeta!(Astroshaper, :DocTestSetup, :(using Astroshaper); recursive=true)
+
+makedocs(;
+    modules=[Astroshaper],
+    repo="https://github.com/MasanoriKanamaru/Astroshaper.jl/blob/{commit}{path}#{line}",
+    sitename="Astroshaper.jl",
+    format=Documenter.HTML(;
+        prettyurls=get(ENV, "CI", "false") == "true",
+        canonical="https://MasanoriKanamaru.github.io/Astroshaper.jl",
+        assets=["assets/favicon.ico"],
+    ),
+    pages=[
+        "Home" => "index.md",
+    ],
+)
+
+deploydocs(;
+    repo="github.com/MasanoriKanamaru/Astroshaper.jl",
+)
