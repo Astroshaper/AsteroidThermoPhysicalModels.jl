51e0a5586e1299a047312172cc2bc93579a17568
hyrodium <hyrodium@gmail.com>
2023-02-14 21:18:28 +0900

    update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -9,12 +9,9 @@ using Statistics
 
 Aqua.test_all(Astroshaper, ambiguities=false)
 
-@testset "Astroshaper.jl" begin
-    # shapepath = "./ryugu_test.obj"
-    # shape = Shape(shapepath; scale=1000, find_visible_facets=true, save_shape=false)
-    # @test typeof(shape) == Shape
-    @test true
+if !Sys.iswindows()
+    # TODO: add test for Windows
+    # `wget` command does not work for Windows, so the testset is disabled for now.
+    include("TPM_Ryugu.jl")
 end
-
-include("TPM_Ryugu.jl")
 include("TPM_Didymos.jl")
