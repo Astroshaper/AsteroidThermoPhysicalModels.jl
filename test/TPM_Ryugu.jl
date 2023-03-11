6de8fa7b8ee86578b87618f2883ee3276f5a250f
kanamaru-masanori <kanamaru-masanori@hotmail.co.jp>
2023-03-11 12:53:40 +0900

    Update TPM_Ryugu.jl

--- test/TPM_Ryugu.jl
+++ test/TPM_Ryugu.jl
@@ -63,13 +63,13 @@
     ##= Load obj file =##
     path_jld = splitext(path_obj)[1]*".jld2"
     if isfile(path_jld)
-        shape = ThermoPhysicalModeling.ShapeModel(path_jld; scale=1000, find_visible_facets=true, save_shape=true)
+        shape = AsteroidThermoPhysicalModels.ShapeModel(path_jld; scale=1000, find_visible_facets=true, save_shape=true)
     else
-        shape = ThermoPhysicalModeling.ShapeModel(path_obj; scale=1000, find_visible_facets=true, save_shape=true)
+        shape = AsteroidThermoPhysicalModels.ShapeModel(path_obj; scale=1000, find_visible_facets=true, save_shape=true)
     end
 
     ##= TPM =##
-    thermo_params = ThermoPhysicalModeling.ThermoParams(
+    thermo_params = AsteroidThermoPhysicalModels.ThermoParams(
         A_B   = 0.04,  # Bolometric Bond albedo
         A_TH  = 0.0,
         k     = 0.1,
@@ -85,7 +85,7 @@
     )
     # Run TPM and save the result
     savepath = "TPM_Ryugu.jld2"
-    ThermoPhysicalModeling.run_TPM!(shape, et_range, sun_ryugu, thermo_params, savepath, save_range)
+    AsteroidThermoPhysicalModels.run_TPM!(shape, et_range, sun_ryugu, thermo_params, savepath, save_range)
     JLD2.jldopen(savepath, "r+") do file
         file["RYUGU_TO_J2000"] = RYUGU_TO_J2000[save_range]
     end
