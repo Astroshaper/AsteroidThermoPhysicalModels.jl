1153d0b1df9fdecee602890f2c2ca1800abc4e5e
kanamaru-masanori <kanamaru-masanori@hotmail.co.jp>
2023-03-11 12:52:40 +0900

    Update Keplerian_motion.jl

--- test/Keplerian_motion.jl
+++ test/Keplerian_motion.jl
@@ -2,8 +2,8 @@
 # These functions are used in https://github.com/Astroshaper/Astroshaper-examples/blob/main/TPM_Kanamaru2021/TPM_Kanamaru2021.ipynb
 
 @testset "Keplerian_motion" begin
-    orbit = ThermoPhysicalModeling.OrbitalElements(ThermoPhysicalModeling.RYUGU)
-    spin = ThermoPhysicalModeling.SpinParams(ThermoPhysicalModeling.RYUGU, orbit)
+    orbit = AsteroidThermoPhysicalModels.OrbitalElements(AsteroidThermoPhysicalModels.RYUGU)
+    spin = AsteroidThermoPhysicalModels.SpinParams(AsteroidThermoPhysicalModels.RYUGU, orbit)
 
     Δt = spin.P / 72
     times = collect(0:Δt:orbit.P)
@@ -11,11 +11,11 @@
     for time in times
         spin_phase = spin.ω * time
 
-        u = ThermoPhysicalModeling.solveKeplerEquation2(orbit, time)
-        r = ThermoPhysicalModeling.get_r(orbit, u)
-        F☉ = ThermoPhysicalModeling.SOLAR_CONST / (norm(r) / ThermoPhysicalModeling.AU)^2
+        u = AsteroidThermoPhysicalModels.solveKeplerEquation2(orbit, time)
+        r = AsteroidThermoPhysicalModels.get_r(orbit, u)
+        F☉ = AsteroidThermoPhysicalModels.SOLAR_CONST / (norm(r) / AsteroidThermoPhysicalModels.AU)^2
         
         r̂☉ = normalize(r) * -1  # Shift the origin from the sun to the body
-        r̂☉ = ThermoPhysicalModeling.orbit_to_body(r̂☉, spin.γ, spin.ε, spin_phase)  # Sun's position in the asteroid-fixed frame
+        r̂☉ = AsteroidThermoPhysicalModels.orbit_to_body(r̂☉, spin.γ, spin.ε, spin_phase)  # Sun's position in the asteroid-fixed frame
     end
 end
