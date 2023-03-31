9f4d8910fdd9295993f1744db63358e6765e4631
Yuto Horikawa <hyrodium@gmail.com>
2023-04-01 01:06:57 +0900

    Fix `Base.show` (#48)
    * fix typo in a comment
    
    * update Base.show
    
    * update Base.show methods
    
    * Fix typo
    
    Co-authored-by: M. Kanamaru <kanamaru-masanori@hotmail.co.jp>
    
    ---------
    
    Co-authored-by: M. Kanamaru <kanamaru-masanori@hotmail.co.jp>

--- src/constants.jl
+++ src/constants.jl
@@ -13,7 +13,7 @@ const σ_SB = 5.670374419e-8            # Stefan–Boltzmann constant [W/m^2/K^4
 #                              Planets
 # ****************************************************************
 
-# TODO: replace these dictionaries with stucts
+# TODO: replace these dictionaries with structs
 """
 # Mercury
 """
