5a04ff5708e1a7a62923631fc5795bfd3578c9c4
Yuto Horikawa <hyrodium@gmail.com>
2023-04-01 10:06:06 +0900

    update direcotry structure in test (#49)
    * update direcotry structure in test
    
    * update gitignore
    
    * update direcotry structure in test
    
    * fix test
    
    * remove directories `test/Didymos` and `test/Ryugu`
    
    * fix paths for obj files
    
    * fix paths for obj files

--- test/TPM_Didymos.jl
+++ test/TPM_Didymos.jl
@@ -1,29 +1,36 @@
 # See https://github.com/Astroshaper/Astroshaper-examples/tree/main/TPM_Didymos for more information.
 @testset "TPM_Didymos" begin
     ##= Download Files =##
-    path_kernels = [
+    paths_kernel = [
         "fk/hera_v07.tf",
         "lsk/naif0012.tls",
         "pck/hera_didymos_v05.tpc",
         "spk/de432s.bsp",
         "spk/didymos_hor_000101_500101_v01.bsp",
         "spk/didymos_gmv_260901_311001_v01.bsp",
-        "dsk/g_50677mm_rad_obj_dida_0000n00000_v001.obj",
-        "dsk/g_06650mm_rad_obj_didb_0000n00000_v001.obj",
     ]
-    
-    for path_kernel in path_kernels
+    paths_shape = [
+        "g_50677mm_rad_obj_dida_0000n00000_v001.obj",
+        "g_06650mm_rad_obj_didb_0000n00000_v001.obj",
+    ]
+
+    for path_kernel in paths_kernel
         url_kernel = "https://s2e2.cosmos.esa.int/bitbucket/projects/SPICE_KERNELS/repos/hera/raw/kernels/$(path_kernel)"
-        file_kernel = joinpath("Didymos", "kernels", path_kernel)
-        mkpath(dirname(file_kernel))
-        isfile(file_kernel) || Downloads.download(url_kernel, file_kernel)
+        filepath = joinpath("kernel", path_kernel)
+        mkpath(dirname(filepath))
+        isfile(filepath) || Downloads.download(url_kernel, filepath)
+    end
+    for path_shape in paths_shape
+        url_kernel = "https://s2e2.cosmos.esa.int/bitbucket/projects/SPICE_KERNELS/repos/hera/raw/kernels/dsk/$(path_shape)"
+        filepath = joinpath("shape", path_shape)
+        mkpath(dirname(filepath))
+        isfile(filepath) || Downloads.download(url_kernel, filepath)
     end
 
     ##= Load data with SPICE =##
-    for path_kernel in path_kernels
-        file_kernel = joinpath("Didymos", "kernels", path_kernel)
-        @show file_kernel, isfile(file_kernel)
-        SPICE.furnsh(file_kernel)
+    for path_kernel in paths_kernel
+        filepath = joinpath("kernel", path_kernel)
+        SPICE.furnsh(filepath)
     end
     et_start = SPICE.utc2et("2027-02-18T00:00:00")
     et_end   = SPICE.utc2et("2027-02-18T01:00:00")
@@ -31,14 +38,14 @@
     et_range = et_start : step : et_end
     @show et_range
     @show length(et_range)
-    
+
     # Indices of et_range to be saved.
     # Save only the last rotation.
     save_range = findall(et_range .> et_range[end] - 7.63262 * 3600)
     @show save_range[begin]
     @show save_range[end]
     @show length(save_range)
-    
+
     # Position 
     sun_d1 = [SPICE.spkpos("SUN", et, "DIDYMOS_FIXED", "None", "DIDYMOS")[1]*1000 for et in et_range]
     sun_d2 = [SPICE.spkpos("SUN", et, "DIMORPHOS_FIXED", "None", "DIMORPHOS")[1]*1000 for et in et_range]
@@ -52,11 +59,11 @@
     SPICE.kclear()
 
     ##= Load obj file =##
-    path_shape1_obj = joinpath("Didymos", "kernels", "dsk", "g_50677mm_rad_obj_dida_0000n00000_v001.obj")
-    path_shape2_obj = joinpath("Didymos", "kernels", "dsk", "g_06650mm_rad_obj_didb_0000n00000_v001.obj")
-    path_shape1_jld = joinpath("Didymos", "kernels", "dsk", "g_50677mm_rad_obj_dida_0000n00000_v001.jld2")
-    path_shape2_jld = joinpath("Didymos", "kernels", "dsk", "g_06650mm_rad_obj_didb_0000n00000_v001.jld2")
-    
+    path_shape1_obj = joinpath("shape", "g_50677mm_rad_obj_dida_0000n00000_v001.obj")
+    path_shape2_obj = joinpath("shape", "g_06650mm_rad_obj_didb_0000n00000_v001.obj")
+    path_shape1_jld = joinpath("shape", "g_50677mm_rad_obj_dida_0000n00000_v001.jld2")
+    path_shape2_jld = joinpath("shape", "g_06650mm_rad_obj_didb_0000n00000_v001.jld2")
+
     if isfile(path_shape1_jld)
         shape1 = AsteroidThermoPhysicalModels.ShapeModel(path_shape1_jld; scale=1000, find_visible_facets=true, save_shape=true)
     else
@@ -86,9 +93,9 @@
 
     AsteroidThermoPhysicalModels.init_temps_zero!(shape1, thermo_params)
     AsteroidThermoPhysicalModels.init_temps_zero!(shape2, thermo_params)
-    
+
     # Run TPM and save the result
-    savepath = joinpath("Didymos", "TPM_Didymos.jld2")
+    savepath = joinpath("TPM_Didymos.jld2")
     shapes = (shape1, shape2)
     suns = (sun_d1, sun_d2)
     AsteroidThermoPhysicalModels.run_TPM!(shapes, et_range, suns, D2_TO_D1, d2_d1, thermo_params, savepath, [:surf_temps, :forces, :torques])
