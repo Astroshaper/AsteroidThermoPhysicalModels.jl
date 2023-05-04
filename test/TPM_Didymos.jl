d654d4a557fc5da47dfd7c280dc706eb4b2b0aea
hyrodium <hyrodium@gmail.com>
2023-05-04 14:25:08 +0900

    use SVector and RotMatrix in TPM_Didymos

--- test/TPM_Didymos.jl
+++ test/TPM_Didymos.jl
@@ -47,15 +47,15 @@
     @show length(save_range)
 
     # Position 
-    sun_d1 = [SPICE.spkpos("SUN", et, "DIDYMOS_FIXED", "None", "DIDYMOS")[1]*1000 for et in et_range]
-    sun_d2 = [SPICE.spkpos("SUN", et, "DIMORPHOS_FIXED", "None", "DIMORPHOS")[1]*1000 for et in et_range]
-    d1_d2 = [SPICE.spkpos("DIDYMOS", et, "DIMORPHOS_FIXED", "None", "DIMORPHOS")[1]*1000 for et in et_range]
-    d2_d1 = [SPICE.spkpos("DIMORPHOS", et, "DIDYMOS_FIXED", "None", "DIDYMOS")[1]*1000 for et in et_range]
+    sun_d1 = [SVector{3}(SPICE.spkpos("SUN", et, "DIDYMOS_FIXED", "None", "DIDYMOS")[1])*1000 for et in et_range]
+    sun_d2 = [SVector{3}(SPICE.spkpos("SUN", et, "DIMORPHOS_FIXED", "None", "DIMORPHOS")[1])*1000 for et in et_range]
+    d1_d2 = [SVector{3}(SPICE.spkpos("DIDYMOS", et, "DIMORPHOS_FIXED", "None", "DIMORPHOS")[1])*1000 for et in et_range]
+    d2_d1 = [SVector{3}(SPICE.spkpos("DIMORPHOS", et, "DIDYMOS_FIXED", "None", "DIDYMOS")[1])*1000 for et in et_range]
     # Transformation matrix
-    D1_TO_D2 = [SPICE.pxform("DIDYMOS_FIXED", "DIMORPHOS_FIXED", et) for et in et_range]
-    D2_TO_D1 = [SPICE.pxform("DIMORPHOS_FIXED", "DIDYMOS_FIXED", et) for et in et_range]
-    D1_TO_J2000 = [SPICE.pxform("DIDYMOS_FIXED", "J2000", et) for et in et_range]
-    D2_TO_J2000 = [SPICE.pxform("DIMORPHOS_FIXED", "J2000", et) for et in et_range]
+    D1_TO_D2 = [RotMatrix{3}(SPICE.pxform("DIDYMOS_FIXED", "DIMORPHOS_FIXED", et)) for et in et_range]
+    D2_TO_D1 = [RotMatrix{3}(SPICE.pxform("DIMORPHOS_FIXED", "DIDYMOS_FIXED", et)) for et in et_range]
+    D1_TO_J2000 = [RotMatrix{3}(SPICE.pxform("DIDYMOS_FIXED", "J2000", et)) for et in et_range]
+    D2_TO_J2000 = [RotMatrix{3}(SPICE.pxform("DIMORPHOS_FIXED", "J2000", et)) for et in et_range]
     SPICE.kclear()
 
     ##= Load obj file =##
