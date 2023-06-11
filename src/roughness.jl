b1b5101c83158b88f0bb308d54fb79436d0ab2a9
kanamaru-masanori <kanamaru-masanori@hotmail.co.jp>
2023-06-12 01:44:12 +0900

    Restore functions to create a topography model of a spherical crater

--- src/roughness.jl
+++ src/roughness.jl
@@ -1,34 +1,67 @@
 
 
-# These functions will be used if the surface roughness models are implemented
-# into the thermophysical simulation.
-
 ################################################################
 #                  Concave spherical segment
 ################################################################
 
-# crater_curvature_radius(r, h) = (r^2 + h^2) / 2h
+"""
+    crater_curvature_radius(r, h) -> R
 
-# function concave_spherical_segment(r, h, xc, yc, x, y)
-#     d² = (x - xc)^2 + (y - yc)^2
-#     d = √d²  # Distance from the crater center
+Return the curvature radius of a concave spherical segment.
 
-#     if d > r
-#         z = 0.
-#     else
-#         R = crater_curvature_radius(r, h)
-#         z = R - h - √(R^2 - d²)
-#     end
-#     z
-# end
+# Arguments
+- `r`: Crater radius
+- `h`: Crater depth
+"""
+crater_curvature_radius(r, h) = (r^2 + h^2) / 2h
 
-# function concave_spherical_segment(r, h; Nx=2^5, Ny=2^5, xc=0.5, yc=0.5)
-#     xs = LinRange(0, 1, Nx + 1)
-#     ys = LinRange(0, 1, Ny + 1)
-#     zs = [concave_spherical_segment(r, h, xc, yc, x, y) for x in xs, y in ys]
+"""
+    concave_spherical_segment(r, h, xc, yc, x, y) -> z
 
-#     xs, ys, zs
-# end
+Return the z-coordinate of a concave spherical segment.
+
+# Arguments
+- `r` : Crater radius
+- `h` : Crater depth
+- `xc`: x-coordinate of crater center
+- `yc`: y-coordinate of crater center
+- `x` : x-coordinate where to calculate z
+- `y` : y-coordinate where to calculate z
+"""
+function concave_spherical_segment(r, h, xc, yc, x, y)
+    d² = (x - xc)^2 + (y - yc)^2
+    d = √d²  # Distance from the crater center
+
+    if d > r
+        z = 0.
+    else
+        R = crater_curvature_radius(r, h)
+        z = R - h - √(R^2 - d²)
+    end
+    z
+end
+
+"""
+    concave_spherical_segment(r, h; Nx=2^5, Ny=2^5, xc=0.5, yc=0.5) -> xs, ys, zs
+
+Return (x, y, z) grid of a concave spherical segment.
+
+# Arguments
+- `r` : Crater radius
+- `h` : Crater depth
+- `Nx`: Number of nodes in the x-direction
+- `Ny`: Number of nodes in the y-direction
+- `xc`: x-coordinate of crater center
+- `yc`: y-coordinate of crater center
+"""
+function concave_spherical_segment(r, h; Nx=2^5, Ny=2^5, xc=0.5, yc=0.5)
+    xs = LinRange(0, 1, Nx + 1)
+    ys = LinRange(0, 1, Ny + 1)
+    zs = [concave_spherical_segment(r, h, xc, yc, x, y) for x in xs, y in ys]
+
+    xs, ys, zs
+end
+
 
 ################################################################
 #                 Parallel sinusoidal trenches
