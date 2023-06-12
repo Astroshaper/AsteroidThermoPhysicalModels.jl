bd11c19989be8a13bc4981564b293f128810e719
kanamaru-masanori <kanamaru-masanori@hotmail.co.jp>
2023-06-12 18:43:31 +0900

    Update find_visiblefacets.jl
    Added a test for loading a spherical crater.

--- test/find_visiblefacets.jl
+++ test/find_visiblefacets.jl
@@ -8,7 +8,7 @@
 
     shape = AsteroidThermoPhysicalModels.load_shape_obj(filepath; scale=1000, find_visible_facets=true)
 
-    ##= Shape model of an icosahedron =##
+    ##= Icosahedron =##
     filepath = "icosahedron.obj"
     shape = AsteroidThermoPhysicalModels.load_shape_obj(filepath; scale=1, find_visible_facets=true)
 
@@ -17,4 +17,12 @@
 
     total_visiblefacets = sum(length(facet.visiblefacets) for facet in shape.facets)
     println("Number of total visible facets: $total_visiblefacets")  # This should be zero for an icosahedron.
+
+    ##= Concave spherical segment =##
+    xs, ys, zs = AsteroidThermoPhysicalModels.concave_spherical_segment(0.4, 0.2; Nx=2^5, Ny=2^5, xc=0.5, yc=0.5)
+    shape = AsteroidThermoPhysicalModels.load_shape_grid(xs, ys, zs; scale=1.0, find_visible_facets=true)
+
+    println("==== Concave spherical segment ====")
+    println("A facet around the crater center:")
+    println(shape.facets[992])
 end
