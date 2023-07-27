8276f95bbafbf99e00b709e32b24a9866c4b8fc4
kanamaru-masanori <kanamaru-masanori@hotmail.co.jp>
2023-07-27 21:36:18 +0900

    Use `Shape.face_forces`

--- src/non_grav.jl
+++ src/non_grav.jl
@@ -17,6 +17,7 @@ function update_thermal_force!(shape::ShapeModel, A_B, ε)
     shape.torque .= 0
     for i in eachindex(shape.faces)
         rᵢ = shape.face_centers[i]
+        r̂ᵢ = normalize(rᵢ)
         n̂ᵢ = shape.face_normals[i]
         aᵢ = shape.face_areas[i]
 
@@ -30,18 +31,16 @@ function update_thermal_force!(shape::ShapeModel, A_B, ε)
         Eᵢ = A_B * F_scat + ε * σ_SB * Tᵢ^4
 
         ## Thermal force on each face
-        @. shape.facets[i].force = - 2/3 * Eᵢ * aᵢ / c₀ * n̂ᵢ      # The first term normal to the face
+        shape.face_forces[i] = - 2/3 * Eᵢ * aᵢ / c₀ * n̂ᵢ      # The first term normal to the face
         for visiblefacet in shape.facets[i].visiblefacets
             fᵢⱼ = visiblefacet.f
             d̂ᵢⱼ = visiblefacet.d̂
 
-            @. shape.facets[i].force += Eᵢ * aᵢ / c₀ * fᵢⱼ * d̂ᵢⱼ  # The second term of self-heating
+            shape.face_forces[i] += Eᵢ * aᵢ / c₀ * fᵢⱼ * d̂ᵢⱼ  # The second term of self-heating
         end
 
         ## Thermal force on the entire shape
-        r̂ᵢ = normalize(rᵢ)
-        dfᵢ = SVector{3}(shape.facets[i].force)
-
+        dfᵢ = shape.face_forces[i]
         shape.force  .+= (r̂ᵢ ⋅ dfᵢ) * r̂ᵢ  # Thermal force
         shape.torque .+= rᵢ × dfᵢ         # Thermal torque
     end
