e92c930d0a8f49ff18f68a767a6e48abbf00cd4f
kanamaru-masanori <kanamaru-masanori@hotmail.co.jp>
2023-08-13 23:34:38 +0900

    Update find_visiblefacets.jl

--- test/find_visiblefacets.jl
+++ test/find_visiblefacets.jl
@@ -9,29 +9,25 @@
     println(msg)
 
     ##= Shape model of Ryugu =##
-    filename = "SHAPE_SFM_49k_v20180804.obj"
-    url_shape = "https://data.darts.isas.jaxa.jp/pub/hayabusa2/paper/Watanabe_2019/$(filename)"
-    filepath = joinpath("shape", filename)
-    isfile(filepath) || Downloads.download(url_shape, filepath)
-
-    shape = AsteroidThermoPhysicalModels.load_shape_obj(filepath; scale=1000, find_visible_facets=true)
+    filepath = joinpath("shape", "ryugu_test.obj")
     println("========  $(filepath)  ========")
+    shape = AsteroidThermoPhysicalModels.load_shape_obj(filepath; scale=1000, find_visible_facets=true)
     println(shape)
 
     ##= Icosahedron =##
     filepath = joinpath("shape", "icosahedron.obj")
-    shape = AsteroidThermoPhysicalModels.load_shape_obj(filepath; scale=1, find_visible_facets=true)
     println("========  $(filepath)  ========")
+    shape = AsteroidThermoPhysicalModels.load_shape_obj(filepath; scale=1, find_visible_facets=true)
     println(shape)
 
     println("Number of total visible facets: ", sum(length.(shape.visiblefacets)))  # This should be zero for an icosahedron.
     println()
 
-    ##= Concave spherical segment =##
+    ##= Concave spherical segment =##]
+    println("========  Concave spherical segment  ========")
     xs, ys, zs = AsteroidThermoPhysicalModels.concave_spherical_segment(0.4, 0.2; Nx=2^5, Ny=2^5, xc=0.5, yc=0.5)
     shape = AsteroidThermoPhysicalModels.load_shape_grid(xs, ys, zs; scale=1.0, find_visible_facets=true)
     
-    println("========  Concave spherical segment  ========")
     println(length(shape.visiblefacets[992]), " faces are visible from the crater center:")
     println()
 end
