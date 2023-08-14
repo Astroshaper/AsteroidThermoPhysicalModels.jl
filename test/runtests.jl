69ad75db0a22fa695ddc8076b9d8790d126e33a4
kanamaru-masanori <kanamaru-masanori@hotmail.co.jp>
2023-08-15 00:20:44 +0900

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -14,7 +14,7 @@ ENABLE_JLD = false
 Aqua.test_all(AsteroidThermoPhysicalModels, ambiguities=false)
 
 include("find_visiblefacets.jl")
-include("heat_conduction_1D.jl")
 include("TPM_Ryugu.jl")
 include("non-uniform_thermoparams.jl")
 include("TPM_Didymos.jl")
+include("heat_conduction_1D.jl")
