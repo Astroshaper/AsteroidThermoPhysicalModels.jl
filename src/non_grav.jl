cf6939bd8e6acd7bfb4b50108b29cc6bde06c7c1
kanamaru-masanori <kanamaru-masanori@hotmail.co.jp>
2023-09-07 14:09:23 +0900

    Update non_grav.jl

--- src/non_grav.jl
+++ src/non_grav.jl
@@ -23,14 +23,17 @@ function update_thermal_force!(stpm::SingleTPM, nₜ::Integer)
         n̂ᵢ = stpm.shape.face_normals[i]
         aᵢ = stpm.shape.face_areas[i]
 
+        F_sun  = stpm.flux[i, 1]
         F_scat = stpm.flux[i, 2]
+        F_rad  = stpm.flux[i, 3]
         Tᵢ = stpm.temperature[begin, i, nₜ]
 
-        ## Total amount of scattered light and radiation, Eᵢ [W/m²].
-        ## Note that both are assumed to be isotropic.
-        A_B = (stpm.thermo_params.A_B isa Real ? stpm.thermo_params.A_B : stpm.thermo_params.A_B[i])
-        ε   = (stpm.thermo_params.ε   isa Real ? stpm.thermo_params.ε   : stpm.thermo_params.ε[i])
-        Eᵢ  = A_B * F_scat + ε * σ_SB * Tᵢ^4
+        ## Total emittance from face i , Eᵢ [W/m²].
+        ## Note that both scattered light and thermal radiation are assumed to be isotropic.
+        A_B  = (stpm.thermo_params.A_B  isa Real ? stpm.thermo_params.A_B  : stpm.thermo_params.A_B[i])
+        A_TH = (stpm.thermo_params.A_TH isa Real ? stpm.thermo_params.A_TH : stpm.thermo_params.A_TH[i])
+        ε    = (stpm.thermo_params.ε    isa Real ? stpm.thermo_params.ε    : stpm.thermo_params.ε[i])
+        Eᵢ  = A_B * F_sun + A_B * F_scat + A_TH * F_rad + ε * σ_SB * Tᵢ^4
 
         ## Thermal force on each face
         stpm.face_forces[i] = - 2/3 * Eᵢ * aᵢ / c₀ * n̂ᵢ      # The first term normal to the face
