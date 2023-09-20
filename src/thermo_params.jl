a2117454dcdd191904f08d9e03e5f8fe7e09695a
kanamaru-masanori <kanamaru-masanori@hotmail.co.jp>
2023-09-21 00:40:04 +0900

    Update thermo_params.jl
    Remove the time-related fields of `UniformThermoParams` and `NonUniformThermoParams`: `t_begin`, `t_end`, `Δt`, `Nt`, and `λ`. These values can be obtained from a given ephemeris time at each time step.

--- src/thermo_params.jl
+++ src/thermo_params.jl
@@ -46,15 +46,9 @@ abstract type AbstractThermoParams end
 - `P`     : Cycle of thermal cycle (rotation period) [sec]
 - `l`     : Thermal skin depth [m]
 - `Γ`     : Thermal inertia [J ⋅ m⁻² ⋅ K⁻¹ ⋅ s⁻⁰⁵ (tiu)]
-- `λ`     : Non-dimensional coefficient for heat diffusion equation
 - `A_B`   : Bond albedo
 - `A_TH`  : Albedo at thermal radiation wavelength
 - `ε`     : Emissivity
-
-- `t_begin` : Start time of the thermophysical simulation [sec]
-- `t_end`   : End time of the thermophysical simulation [sec]
-- `Δt`      : Timestep [sec]
-- `Nt`      : Number of timesteps
 
 - `z_max` : Depth of the bottom of a heat conduction equation [m]
 - `Δz`    : Depth step width [m]
@@ -64,15 +58,9 @@ struct NonUniformThermoParams <: AbstractThermoParams
     P       ::Float64          # Common for all faces
     l       ::Vector{Float64}
     Γ       ::Vector{Float64}
-    λ       ::Vector{Float64}
     A_B     ::Vector{Float64}
     A_TH    ::Vector{Float64}
     ε       ::Vector{Float64}
-
-    t_begin ::Float64          # Common for all faces
-    t_end   ::Float64          # Common for all faces
-    Δt      ::Float64          # Common for all faces
-    Nt      ::Int              # Common for all faces
 
     z_max   ::Float64          # Common for all faces
     Δz      ::Float64          # Common for all faces
@@ -86,15 +74,9 @@ end
 - `P`     : Thermal cycle (rotation period) [sec]
 - `l`     : Thermal skin depth [m]
 - `Γ`     : Thermal inertia [J ⋅ m⁻² ⋅ K⁻¹ ⋅ s⁻⁰⁵ (tiu)]
-- `λ`     : Non-dimensional coefficient for heat diffusion equation
 - `A_B`   : Bond albedo
 - `A_TH`  : Albedo at thermal radiation wavelength
 - `ε`     : Emissivity
-
-- `t_begin` : Start time of the thermophysical simulation [sec]
-- `t_end`   : End time of the thermophysical simulation [sec]
-- `Δt`      : Timestep [sec]
-- `Nt`      : Number of timesteps
 
 - `z_max` : Depth of the bottom of a heat conduction equation [m]
 - `Δz`    : Depth step width [m]
@@ -104,15 +86,9 @@ struct UniformThermoParams <: AbstractThermoParams
     P       ::Float64
     l       ::Float64
     Γ       ::Float64
-    λ       ::Float64
     A_B     ::Float64
     A_TH    ::Float64
     ε       ::Float64
-
-    t_begin ::Float64
-    t_end   ::Float64
-    Δt      ::Float64
-    Nt      ::Int
 
     z_max   ::Float64
     Δz      ::Float64
@@ -123,15 +99,10 @@ end
 """
     thermoparams(; A_B, A_TH, k, ρ, Cp, ε, t_begin, t_end, Nt, z_max, Nz, P)
 """
-function thermoparams(; P, l, Γ, A_B, A_TH, ε, t_begin, t_end, Nt, z_max, Nz)
+function thermoparams(; P, l, Γ, A_B, A_TH, ε, z_max, Nz)
 
-    Δt = (t_end - t_begin) / (Nt - 1)
     Δz = z_max / (Nz - 1)
-
-    λ = @. (Δt/P) / (Δz/l)^2 / 4π
-    maximum(λ) > 0.5 && error("λ should be smaller than 0.5 for convergence of the forward Euler method.")
-
-    LENGTH = maximum(length, [A_B, A_TH, ε, l, Γ, λ])
+    LENGTH = maximum(length, [A_B, A_TH, ε, l, Γ])
 
     if LENGTH > 1
         A_B   isa Real && (A_B  = fill(A_B,  LENGTH))
@@ -139,11 +110,10 @@ function thermoparams(; P, l, Γ, A_B, A_TH, ε, t_begin, t_end, Nt, z_max, Nz)
         ε     isa Real && (ε    = fill(ε,    LENGTH))
         l     isa Real && (l    = fill(l,    LENGTH))
         Γ     isa Real && (Γ    = fill(Γ,    LENGTH))
-        λ     isa Real && (λ    = fill(λ,    LENGTH))
         
-        NonUniformThermoParams(P, l, Γ, λ, A_B, A_TH, ε, t_begin, t_end, Δt, Nt, z_max, Δz, Nz)
+        NonUniformThermoParams(P, l, Γ, A_B, A_TH, ε, z_max, Δz, Nz)
     else
-        UniformThermoParams(P, l, Γ, λ, A_B, A_TH, ε, t_begin, t_end, Δt, Nt, z_max, Δz, Nz)
+        UniformThermoParams(P, l, Γ, A_B, A_TH, ε, z_max, Δz, Nz)
     end
 end
 
@@ -158,21 +128,10 @@ function Base.show(io::IO, params::UniformThermoParams)
     msg *= "          = $(SPICE.convrt(params.P, "seconds", "hours")) [h]\n"
     msg *= "  l       = $(params.l) [m]\n"
     msg *= "  Γ       = $(params.Γ) [tiu]\n"
-    msg *= "  λ       = $(params.λ)\n"
     msg *= "  A_B     = $(params.A_B)\n"
     msg *= "  A_TH    = $(params.A_TH)\n"
     msg *= "  ε       = $(params.ε)\n"
-    
-    msg *= "-----------------------------------\n"
-
-    msg *= "  t_begin = $(params.t_begin) [sec]\n"
-    msg *= "          = $(params.t_begin / params.P) [P]\n"
-    msg *= "  t_end   = $(params.t_end) [sec]\n"
-    msg *= "          = $(params.t_end / params.P) [P]\n"
-    msg *= "  Δt      = $(params.Δt) [sec]\n"
-    msg *= "          = $(params.Δt / params.P) [P]\n"
-    msg *= "  Nt      = $(params.Nt)\n"
-
+  
     msg *= "-----------------------------------\n"
 
     msg *= "  z_max   = $(params.z_max) [m]\n"
