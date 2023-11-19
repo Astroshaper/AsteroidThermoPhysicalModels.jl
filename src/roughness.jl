ae414e8995c1bc16482a8dfc1f07f16e9651c7d0
Yuto Horikawa <hyrodium@gmail.com>
2023-11-19 19:11:07 +0900

    add more argument types (#100)

--- src/roughness.jl
+++ src/roughness.jl
@@ -13,7 +13,7 @@ Return the curvature radius of a concave spherical segment.
 - `r`: Crater radius
 - `h`: Crater depth
 """
-crater_curvature_radius(r, h) = (r^2 + h^2) / 2h
+crater_curvature_radius(r::Real, h::Real) = (r^2 + h^2) / 2h
 
 """
     concave_spherical_segment(r, h, xc, yc, x, y) -> z
@@ -28,7 +28,7 @@ Return the z-coordinate of a concave spherical segment.
 - `x` : x-coordinate where to calculate z
 - `y` : y-coordinate where to calculate z
 """
-function concave_spherical_segment(r, h, xc, yc, x, y)
+function concave_spherical_segment(r::Real, h::Real, xc::Real, yc::Real, x::Real, y::Real)
     d² = (x - xc)^2 + (y - yc)^2
     d = √d²  # Distance from the crater center
 
@@ -54,7 +54,7 @@ Return (x, y, z) grid of a concave spherical segment.
 - `xc`: x-coordinate of crater center
 - `yc`: y-coordinate of crater center
 """
-function concave_spherical_segment(r, h; Nx=2^5, Ny=2^5, xc=0.5, yc=0.5)
+function concave_spherical_segment(r::Real, h::Real; Nx::Integer=2^5, Ny::Integer=2^5, xc::Real=0.5, yc::Real=0.5)
     xs = LinRange(0, 1, Nx + 1)
     ys = LinRange(0, 1, Ny + 1)
     zs = [concave_spherical_segment(r, h, xc, yc, x, y) for x in xs, y in ys]
