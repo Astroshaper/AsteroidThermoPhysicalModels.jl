5334edc963ab7c64e94eec93c363b6e876fe7e04
Yuto Horikawa <hyrodium@gmail.com>
2023-12-10 19:49:17 +0900

    Remove unnecessary `static` keyword (#110)
    * remove unnecessary `static` keyword
    
    * Apply suggestions from code review
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/shape.jl
+++ src/shape.jl
@@ -55,9 +55,9 @@ function Base.show(io::IO, shape::ShapeModel)
     print(io, msg)
 end
 
-function load_shape_obj(shapepath; scale=1.0, find_visible_facets=false)
+function load_shape_obj(shapepath; scale = 1.0, find_visible_facets = false)
     # TODO: use MeshIO.jl
-    nodes, faces = loadobj(shapepath; scale=scale, static=true, message=false)
+    nodes, faces = loadobj(shapepath; scale = scale, message = false)
 
     face_centers = [face_center(nodes[face]) for face in faces]
     face_normals = [face_normal(nodes[face]) for face in faces]
