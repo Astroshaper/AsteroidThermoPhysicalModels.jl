7290429adea8f0ce18cbeba6b94ba49a6dccc37b
M. Kanamaru <kanamaru-masanori@hotmail.co.jp>
2024-07-06 15:22:45 +0900

    Use `NaN` for `E_cons` instead of `missing` (#136)
    * Update TPM.jl
    
    * Update TPM.jl

--- src/TPM.jl
+++ src/TPM.jl
@@ -252,7 +252,7 @@ struct SingleTPMResult
     times  ::Vector{Float64}
     E_in   ::Vector{Float64}
     E_out  ::Vector{Float64}
-    E_cons ::Vector{Union{Float64, Missing}}
+    E_cons ::Vector{Float64}
     force  ::Vector{SVector{3, Float64}}
     torque ::Vector{SVector{3, Float64}}
 
@@ -280,7 +280,7 @@ function SingleTPMResult(stpm::SingleTPM, ephem, times_to_save::Vector{Float64},
 
     E_in   = zeros(n_step)
     E_out  = zeros(n_step)
-    E_cons = Vector{Union{Float64, Missing}}(missing, n_step)
+    E_cons = fill(NaN, n_step)
     force  = zeros(SVector{3, Float64}, n_step)
     torque = zeros(SVector{3, Float64}, n_step)
 
