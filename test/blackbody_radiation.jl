cc055692c90ab4958928436947c3750fc45d16da
kanamaru-masanori <kanamaru-masanori@hotmail.co.jp>
2024-07-22 08:08:18 +0900

    Add test for `radiation.jl`

---
+++ test/blackbody_radiation.jl
@@ -0,0 +1,11 @@
+@testset "blackbody_radiation" begin
+
+    msg = """\n
+    ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+    |               Test: blackbody_radiation                |
+    ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
+    """
+    println(msg)
+    
+
+end
