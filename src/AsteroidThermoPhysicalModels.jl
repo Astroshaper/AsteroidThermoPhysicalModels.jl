4db0460f7a15e3e3592438a4b4f3dfa62e9ac8be
kanamaru-masanori <kanamaru-masanori@hotmail.co.jp>
2024-07-22 08:08:00 +0900

    Add `radiation.jl`

--- src/AsteroidThermoPhysicalModels.jl
+++ src/AsteroidThermoPhysicalModels.jl
@@ -18,6 +18,8 @@ import GeometryBasics
 const SOLAR_CONST = 1366.0   # Solar constant, Φ☉ [W/m^2]
 const c₀ = 299792458.0       # Speed of light [m/s]
 const σ_SB = 5.670374419e-8  # Stefan–Boltzmann constant [W/m^2/K^4]
+const h = 6.62607015e-34     # Planck constant [J⋅s]
+const k_B = 1.380649e-23     # Boltzmann constant [J/K]
 
 include("obj.jl")
 include("shape.jl")
@@ -29,6 +31,7 @@ include("TPM.jl")
 include("heat_conduction.jl")
 include("energy_flux.jl")
 include("non_grav.jl")
+include("radiation.jl")
 export thermal_skin_depth, thermal_inertia, init_temperature!, run_TPM!
 
 include("roughness.jl")
