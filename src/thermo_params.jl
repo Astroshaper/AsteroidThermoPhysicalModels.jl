8e33efc1cf5697274e7243ae1bd631622f139dba
kanamaru-masanori <kanamaru-masanori@hotmail.co.jp>
2024-08-16 11:52:13 +0900

    Update thermo_params.jl
    Unify `UniformThermoParams` and `NonUniformThermoParams `

--- src/thermo_params.jl
+++ src/thermo_params.jl
@@ -40,55 +40,27 @@ thermal_inertia(k, ρ, Cₚ) = @. √(k * ρ * Cₚ)
 abstract type AbstractThermoParams end
 
 """
-    struct NonUniformThermoParams
+    struct ThermoParams
 
 # Fields
-- `period`    : Cycle of thermal cycle (rotation period) [sec]
-- `skindepth` : Thermal skin depth [m]
-- `inertia` : Thermal inertia [J ⋅ m⁻² ⋅ K⁻¹ ⋅ s⁻⁰⁵ (tiu)]
-- `reflectance_vis` : reflectances for visible light
-- `reflectance_ir`  : reflectances for thermal infrared
-- `emissivity` : Emissivity [-]
+- `period`          : Cycle of thermal cycle (rotation period) [sec]
+- `skindepth`       : Thermal skin depth for each facet [m]
+- `inertia`         : Thermal inertia for each facet [thermal inertia unit (tiu)]
+- `reflectance_vis` : Reflectance in visible light for each facet [-]
+- `reflectance_ir`  : Reflectance in thermal infrared for each facet [-]
+- `emissivity`      : Emissivity for each facet [-]
 
-- `z_max` : Depth of the bottom of a heat conduction equation [m]
-- `Δz`    : Depth step width [m]
+- `z_max`   : Depth of the lower boundary of a heat conduction equation [m]
+- `Δz`      : Depth step width [m]
 - `n_depth` : Number of depth steps
 """
-struct NonUniformThermoParams <: AbstractThermoParams
-    period  ::Float64          # Common for all faces
-    skindepth::Vector{Float64}
-    inertia ::Vector{Float64}
-    reflectance_vis   ::Vector{Float64}
-    reflectance_ir    ::Vector{Float64}
-    emissivity::Vector{Float64}
-
-    z_max   ::Float64          # Common for all faces
-    Δz      ::Float64          # Common for all faces
-    n_depth ::Int              # Common for all faces
-end
-
-"""
-    struct UniformThermoParams
-
-# Fields
-- `period`: Thermal cycle (rotation period) [sec]
-- `skindepth`: Thermal skin depth [m]
-- `inertia`  : Thermal inertia [J ⋅ m⁻² ⋅ K⁻¹ ⋅ s⁻⁰⁵ (tiu)]
-- `reflectance_vis` : reflectances for visible light
-- `reflectance_ir`  : reflectances for thermal infrared
-- `emissivity` : Emissivity [-]
-
-- `z_max` : Depth of the bottom of a heat conduction equation [m]
-- `Δz`    : Depth step width [m]
-- `n_depth`: Number of depth steps
-"""
-struct UniformThermoParams <: AbstractThermoParams
-    period  ::Float64
-    skindepth::Float64
-    inertia ::Float64
-    reflectance_vis ::Float64
-    reflectance_ir  ::Float64
-    emissivity::Float64
+struct ThermoParams <: AbstractThermoParams
+    period          ::Float64
+    skindepth       ::Vector{Float64}
+    inertia         ::Vector{Float64}
+    reflectance_vis ::Vector{Float64}
+    reflectance_ir  ::Vector{Float64}
+    emissivity      ::Vector{Float64}
 
     z_max   ::Float64
     Δz      ::Float64
@@ -96,52 +68,60 @@ struct UniformThermoParams <: AbstractThermoParams
 end
 
 
-"""
-    thermoparams(; P, l, Γ, R_vis, R_ir, ε, z_max, n_depth)
-"""
-function thermoparams(; P, l, Γ, R_vis, R_ir, ε, z_max, n_depth)
-
-    Δz = z_max / (n_depth - 1)
-    LENGTH = maximum(length, [R_vis, R_ir, ε, l, Γ])
-
-    if LENGTH > 1
-        R_vis   isa Real && (R_vis  = fill(R_vis,  LENGTH))
-        R_ir  isa Real && (R_ir = fill(R_ir, LENGTH))
-        ε     isa Real && (ε    = fill(ε,    LENGTH))
-        l     isa Real && (l    = fill(l,    LENGTH))
-        Γ     isa Real && (Γ    = fill(Γ,    LENGTH))
-        
-        NonUniformThermoParams(P, l, Γ, R_vis, R_ir, ε, z_max, Δz, n_depth)
-    else
-        UniformThermoParams(P, l, Γ, R_vis, R_ir, ε, z_max, Δz, n_depth)
-    end
-end
+# function thermoparams(;
+#     P,
+#     l::Vector{Float64},
+#     Γ::Vector{Float64},
+#     R_vis::Vector{Float64},
+#     R_ir::Vector{Float64},
+#     ε::Vector{Float64},
+#     z_max,
+#     n_depth
+# )
+#     Δz = z_max / (n_depth - 1)    
+#     return ThermoParams(P, l, Γ, R_vis, R_ir, ε, z_max, Δz, n_depth)
+# end
 
 
-function Base.show(io::IO, params::UniformThermoParams)
+# function thermoparams(;
+#     P,
+#     l::Float64,
+#     Γ::Float64,
+#     R_vis::Float64,
+#     R_ir::Float64,
+#     ε::Float64,
+#     z_max,
+#     n_depth
+# )
+#     Δz = z_max / (n_depth - 1)    
+#     return ThermoParams(P, [l], [Γ], [R_vis], [R_ir], [ε], z_max, Δz, n_depth)
+# end
 
-    msg =  "⋅-----------------------------------⋅\n"
-    msg *= "|     Thermophysical parameters     |\n"
-    msg *= "⋅-----------------------------------⋅\n"
 
-    msg *= "  P       = $(params.period) [sec]\n"
-    msg *= "          = $(SPICE.convrt(params.period, "seconds", "hours")) [h]\n"
-    msg *= "  l       = $(params.skindepth) [m]\n"
-    msg *= "  Γ       = $(params.inertia) [tiu]\n"
-    msg *= "  R_vis   = $(params.reflectance_vis)\n"
-    msg *= "  R_ir    = $(params.reflectance_ir)\n"
-    msg *= "  ε       = $(params.emissivity)\n"
+# function Base.show(io::IO, params::ThermoParams)
+
+#     msg =  "⋅-----------------------------------⋅\n"
+#     msg *= "|     Thermophysical parameters     |\n"
+#     msg *= "⋅-----------------------------------⋅\n"
+
+#     msg *= "  Rotation period        = $(params.period) [sec]\n"
+#     msg *= "                         = $(SPICE.convrt(params.period, "seconds", "hours")) [h]\n"
+#     msg *= "  Thermal skindepth      = $(params.skindepth) [m]\n"
+#     msg *= "  Thermal inertia        = $(params.inertia) [tiu]\n"
+#     msg *= "  Reflectance (visible)  = $(params.reflectance_vis)\n"
+#     msg *= "  Reflectance (infrared) = $(params.reflectance_ir)\n"
+#     msg *= "  Emissivity             = $(params.emissivity)\n"
   
-    msg *= "-----------------------------------\n"
+#     msg *= "-----------------------------------\n"
 
-    msg *= "  z_max   = $(params.z_max) [m]\n"
-    msg *= "          = $(params.z_max / params.skindepth) [l]\n"
-    msg *= "  Δz      = $(params.Δz) [m]\n"
-    msg *= "          = $(params.Δz / params.skindepth) [l]\n"
-    msg *= "  n_depth = $(params.n_depth)\n"
+#     msg *= "  Maximum depth       = $(params.z_max) [m]\n"
+#     msg *= "                      = $(params.z_max / params.skindepth) [l]\n"
+#     msg *= "  Depth step width    = $(params.Δz) [m]\n"
+#     msg *= "                      = $(params.Δz / params.skindepth) [l]\n"
+#     msg *= "  Num. of depth nodes = $(params.n_depth)\n"
     
-    msg *= "-----------------------------------\n"
+#     msg *= "-----------------------------------\n"
     
-    print(io, msg)
-end
+#     print(io, msg)
+# end
 
