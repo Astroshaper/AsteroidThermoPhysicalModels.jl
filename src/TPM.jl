99b1b0623cca45553162b4921c78b8d443559ca5
mkanamaru <kanamaru-masanori@hotmail.co.jp>
2025-04-16 14:55:14 +0200

    Update TPM.jl
    Remove `E_cons` from `SingleTPMResult` because `period` was removed from `ThermoParams`.

--- src/TPM.jl
+++ src/TPM.jl
@@ -117,19 +117,21 @@ Broadcast the thermophysical parameters to all faces if the values are uniform g
 - `n_face`        : Number of faces on the shape model
 """
 function broadcast_thermo_params!(thermo_params::ThermoParams, n_face::Int)
-    if length(thermo_params.inertia) == 1
-        resize!(thermo_params.skindepth,       n_face)
-        resize!(thermo_params.inertia,         n_face)
-        resize!(thermo_params.reflectance_vis, n_face)
-        resize!(thermo_params.reflectance_ir,  n_face)
-        resize!(thermo_params.emissivity,      n_face)
+    if length(thermo_params.thermal_conductivity) == 1
+        resize!(thermo_params.thermal_conductivity, n_face)
+        resize!(thermo_params.density,              n_face)
+        resize!(thermo_params.heat_capacity,        n_face)
+        resize!(thermo_params.reflectance_vis,      n_face)
+        resize!(thermo_params.reflectance_ir,       n_face)
+        resize!(thermo_params.emissivity,           n_face)
 
-        thermo_params.skindepth[2:end]       .= thermo_params.skindepth[begin]
-        thermo_params.inertia[2:end]         .= thermo_params.inertia[begin]
-        thermo_params.reflectance_vis[2:end] .= thermo_params.reflectance_vis[begin]
-        thermo_params.reflectance_ir[2:end]  .= thermo_params.reflectance_ir[begin]
-        thermo_params.emissivity[2:end]      .= thermo_params.emissivity[begin]
-    elseif length(thermo_params.inertia) == n_face
+        thermo_params.thermal_conductivity[2:end] .= thermo_params.thermal_conductivity[begin]
+        thermo_params.density[2:end]              .= thermo_params.density[begin]
+        thermo_params.heat_capacity[2:end]        .= thermo_params.heat_capacity[begin]
+        thermo_params.reflectance_vis[2:end]      .= thermo_params.reflectance_vis[begin]
+        thermo_params.reflectance_ir[2:end]       .= thermo_params.reflectance_ir[begin]
+        thermo_params.emissivity[2:end]           .= thermo_params.emissivity[begin]
+    elseif length(thermo_params.thermal_conductivity) == n_face
         # Do nothing
     else
         throw(ArgumentError("The length of the thermophysical parameters is invalid."))
@@ -278,7 +280,6 @@ Output data format for `SingleAsteroidThermoPhysicalModel`
 - `times`  : Timesteps, given the same vector as `ephem.time` [s]
 - `E_in`   : Input energy per second on the whole surface [W]
 - `E_out`  : Output enegey per second from the whole surface [W]
-- `E_cons` : Energy conservation ratio [-], ratio of total energy going out to total energy coming in in the last rotation cycle
 - `force`  : Thermal force on the asteroid [N]
 - `torque` : Thermal torque on the asteroid [N ⋅ m]
 
@@ -299,7 +300,6 @@ struct SingleAsteroidThermoPhysicalModelResult
     times  ::Vector{Float64}
     E_in   ::Vector{Float64}
     E_out  ::Vector{Float64}
-    E_cons ::Vector{Float64}
     force  ::Vector{SVector{3, Float64}}
     torque ::Vector{SVector{3, Float64}}
 
@@ -327,7 +327,6 @@ function SingleAsteroidThermoPhysicalModelResult(stpm::SingleAsteroidThermoPhysi
 
     E_in   = zeros(n_step)
     E_out  = zeros(n_step)
-    E_cons = fill(NaN, n_step)
     force  = zeros(SVector{3, Float64}, n_step)
     torque = zeros(SVector{3, Float64}, n_step)
 
@@ -342,7 +341,6 @@ function SingleAsteroidThermoPhysicalModelResult(stpm::SingleAsteroidThermoPhysi
         ephem.time,
         E_in,
         E_out,
-        E_cons,
         force,
         torque,
         times_to_save,
@@ -403,18 +401,7 @@ function update_TPM_result!(result::SingleAsteroidThermoPhysicalModelResult, stp
     result.force[i_time]  = stpm.force
     result.torque[i_time] = stpm.torque
 
-    P  = stpm.thermo_params.period # Rotation period
-    t  = result.times[i_time]      # Current time
-    t₀ = result.times[begin]   # Time at the beginning of the simulation
-
-    if t > t₀ + P  # Note that `E_cons` cannot be calculated during the first rotation
-        n_step_in_period = count(@. t - P ≤ result.times < t)  # Number of time steps within the last rotation
-
-        ΣE_in  = sum(result.E_in[n-1]  * (result.times[n] - result.times[n-1]) for n in (i_time - n_step_in_period + 1):i_time)
-        ΣE_out = sum(result.E_out[n-1] * (result.times[n] - result.times[n-1]) for n in (i_time - n_step_in_period + 1):i_time)
-
-        result.E_cons[i_time] = ΣE_out / ΣE_in
-    end
+    t  = result.times[i_time]  # Current time
 
     if t in result.times_to_save  # In the step of saving temperature
         i_time_save = findfirst(isequal(t), result.times_to_save)
@@ -469,7 +456,6 @@ function export_TPM_results(dirpath, result::SingleAsteroidThermoPhysicalModelRe
     df.time     = result.times
     df.E_in     = result.E_in
     df.E_out    = result.E_out
-    df.E_cons   = result.E_cons
     df.force_x  = [f[1] for f in result.force]
     df.force_y  = [f[2] for f in result.force]
     df.force_z  = [f[3] for f in result.force]
