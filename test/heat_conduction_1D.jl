37c6759e1af257fb27c645760b6ff2fa27c71360
mkanamaru <kanamaru-masanori@hotmail.co.jp>
2025-04-30 15:39:01 +0200

    Update heat_conduction_1D.jl

--- test/heat_conduction_1D.jl
+++ test/heat_conduction_1D.jl
@@ -40,8 +40,8 @@
     BC_UPPER       = AsteroidThermoPhysicalModels.IsothermalBoundaryCondition(0)
     BC_LOWER       = AsteroidThermoPhysicalModels.IsothermalBoundaryCondition(0)
 
-    stpm_FE = AsteroidThermoPhysicalModels.SingleAsteroidTPM(shape, thermo_params; SELF_SHADOWING, SELF_HEATING, BC_UPPER, BC_LOWER, SOLVER=AsteroidThermoPhysicalModels.ExplicitEulerSolver(thermo_params))
-    stpm_BE = AsteroidThermoPhysicalModels.SingleAsteroidTPM(shape, thermo_params; SELF_SHADOWING, SELF_HEATING, BC_UPPER, BC_LOWER, SOLVER=AsteroidThermoPhysicalModels.ImplicitEulerSolver(thermo_params))
+    stpm_EE = AsteroidThermoPhysicalModels.SingleAsteroidTPM(shape, thermo_params; SELF_SHADOWING, SELF_HEATING, BC_UPPER, BC_LOWER, SOLVER=AsteroidThermoPhysicalModels.ExplicitEulerSolver(thermo_params))
+    stpm_IE = AsteroidThermoPhysicalModels.SingleAsteroidTPM(shape, thermo_params; SELF_SHADOWING, SELF_HEATING, BC_UPPER, BC_LOWER, SOLVER=AsteroidThermoPhysicalModels.ImplicitEulerSolver(thermo_params))
     stpm_CN = AsteroidThermoPhysicalModels.SingleAsteroidTPM(shape, thermo_params; SELF_SHADOWING, SELF_HEATING, BC_UPPER, BC_LOWER, SOLVER=AsteroidThermoPhysicalModels.CrankNicolsonSolver(thermo_params))
 
     ##= Initial temperature =##
@@ -49,8 +49,8 @@
     xs = [thermo_params.Δz * (i-1) for i in 1:thermo_params.n_depth]
     Ts = [T₀(x) for x in xs]
 
-    stpm_FE.temperature .= Ts
-    stpm_BE.temperature .= Ts
+    stpm_EE.temperature .= Ts
+    stpm_IE.temperature .= Ts
     stpm_CN.temperature .= Ts
 
     ##= Run TPM =##
@@ -58,8 +58,8 @@
         i_time == length(et_range) && break  # Stop to update the temperature at the final step
         Δt = ephem.time[i_time+1] - ephem.time[i_time]
         
-        AsteroidThermoPhysicalModels.explicit_euler!(stpm_FE, Δt)
-        AsteroidThermoPhysicalModels.implicit_euler!(stpm_BE, Δt)
+        AsteroidThermoPhysicalModels.explicit_euler!(stpm_EE, Δt)
+        AsteroidThermoPhysicalModels.implicit_euler!(stpm_IE, Δt)
         AsteroidThermoPhysicalModels.crank_nicolson!(stpm_CN, Δt)
     end
 
@@ -83,18 +83,42 @@
     end
 
     @testset "Solver comparison" begin
-        δ_max_FE_BE = maximum(relative_error.(stpm_FE.temperature, stpm_BE.temperature))
-        δ_max_FE_CN = maximum(relative_error.(stpm_FE.temperature, stpm_CN.temperature))
-        δ_max_BE_CN = maximum(relative_error.(stpm_BE.temperature, stpm_CN.temperature))
+        ## Maximum relative error between solvers
+        δ_max_EE_IE = maximum(relative_error.(stpm_EE.temperature, stpm_IE.temperature))
+        δ_max_EE_CN = maximum(relative_error.(stpm_EE.temperature, stpm_CN.temperature))
+        δ_max_IE_CN = maximum(relative_error.(stpm_IE.temperature, stpm_CN.temperature))
         
         ## Allowable relative error
-        @test δ_max_FE_BE < 0.01
-        @test δ_max_FE_CN < 0.01
-        @test δ_max_BE_CN < 0.01
+        @test δ_max_EE_IE < 0.01
+        @test δ_max_EE_CN < 0.01
+        @test δ_max_IE_CN < 0.01
         
         println("Maximum relative errors:")
-        println("    - Explicit Euler vs. Implicit Euler : ", δ_max_FE_BE)
-        println("    - Explicit Euler vs. Crank-Nicolson : ", δ_max_FE_CN)
-        println("    - Implicit Euler vs. Crank-Nicolson : ", δ_max_BE_CN)
+        println("    - Explicit Euler vs. Implicit Euler : ", δ_max_EE_IE)
+        println("    - Explicit Euler vs. Crank-Nicolson : ", δ_max_EE_CN)
+        println("    - Implicit Euler vs. Crank-Nicolson : ", δ_max_IE_CN)
+    end
+
+    @testset "Numercal vs. analytical solution (isothermal boundary condition)" begin
+        T_analytical = similar(Ts)
+        α = k / (ρ * Cₚ)  # Thermal diffusivity [m²/s]
+        for (i, x) in enumerate(xs)
+            T_analytical[i] = AsteroidThermoPhysicalModels.analytical_solution_isothermal(x, ephem.time[end], z_max, α)
+        end
+
+        # Maximum relative error between numerical and analytical solutions
+        δ_max_EE = maximum(relative_error.(stpm_EE.temperature[:, 1], T_analytical))
+        δ_max_IE = maximum(relative_error.(stpm_IE.temperature[:, 1], T_analytical))
+        δ_max_CN = maximum(relative_error.(stpm_CN.temperature[:, 1], T_analytical))
+
+        ## Allowable relative error
+        @test δ_max_EE < 0.01
+        @test δ_max_IE < 0.01
+        @test δ_max_CN < 0.01
+
+        println("Maximum relative errors:")
+        println("    - Explicit Euler vs. Isothermal analytical solution: ", δ_max_EE)
+        println("    - Implicit Euler vs. Isothermal analytical solution: ", δ_max_IE)
+        println("    - Crank-Nicolson vs. Isothermal analytical solution: ", δ_max_CN)
     end
 end
